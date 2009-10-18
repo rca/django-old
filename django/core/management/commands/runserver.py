0d877e83eb862da195200241bb91e84181b7783b
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2009-10-18 09:53:44 -0700

    Reverted "Added call to error_file() to track bad files."
    This reverts commit e1d0be02b001155a09b012f4867de1efd14454b4.  Error checking
    logic is now contained within the autoreload module.

--- django/core/management/commands/runserver.py
+++ django/core/management/commands/runserver.py
@@ -45,14 +45,7 @@ class Command(BaseCommand):
             from django.conf import settings
             from django.utils import translation
             print "Validating models..."
-            try:
-                self.validate(display_num_errors=True)
-            except (IndentationError, SyntaxError), msg:
-                if use_reloader:
-                    from django.utils import autoreload
-                    exc_info = sys.exc_info()
-                    autoreload.error_file(exc_info)
-                raise
+            self.validate(display_num_errors=True)
             print "\nDjango version %s, using settings %r" % (django.get_version(), settings.SETTINGS_MODULE)
             print "Development server is running at http://%s:%s/" % (addr, port)
             print "Quit the server with %s." % quit_command
